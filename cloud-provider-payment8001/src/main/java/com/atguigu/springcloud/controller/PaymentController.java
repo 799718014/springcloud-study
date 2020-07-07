69f3204410ac0ef5aaa9ec712a2c8f608edd552c
liuzx <liuzx@usoftchina.com>
2020-07-07 17:12:51 +0800

    1、eureka配置调整 2、eureka集群 3、支付模块集群

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -21,14 +22,17 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
     /*    log.info("********插入数据*****");*/
         if(result > 0){
-            return new CommonResult(200,"插入数据库成功");
+            return new CommonResult(200,"插入数据库成功.serverPort"+serverPort,result);
         }else{
-            return new CommonResult(-1,"插入数据库失败");
+            return new CommonResult(-1,"插入数据库失败",null);
         }
     }
 
@@ -39,7 +43,7 @@ public class PaymentController {
         if(payment == null){
             return new CommonResult(200,"id没有对应记录",null);
         }else{
-            return new CommonResult(200,"查询成功55",payment);
+            return new CommonResult(200,"查询成功55.serverPort"+serverPort,payment);
         }
     }
 }
