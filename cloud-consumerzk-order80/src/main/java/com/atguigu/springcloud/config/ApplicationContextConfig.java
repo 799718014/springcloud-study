22d6ca53fa31feb980da47971ada9ac86c7d62c5
liuzx <liuzx@usoftchina.com>
2020-07-09 18:21:39 +0800

    1、zookeeper 配置和开发

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * [用一句话描述此类]
+ *
+ * @author: liuzx
+ * @create: 2020-07-09 18:08
+ **/
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
