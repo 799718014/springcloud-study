5735570aa01b2ebde916d32cb0d2fe3005d463d6
liuzx <liuzx@usoftchina.com>
2020-08-18 23:48:16 +0800

    1、zipkin 的配置和开发

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/springcloud/controller/OrderController.java
@@ -50,4 +50,11 @@ public class OrderController {
         }
 
     }
+
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin(){
+
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/zipkin",String.class);
+    }
+
 }
