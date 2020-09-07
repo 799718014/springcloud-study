2faafe416d0c622eda44e12e0af9fcd626f1dc6e
liuzx <liuzx@usoftchina.com>
2020-09-07 23:43:27 +0800

    1、sentinel 流控规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibab/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibab/controller/FlowLimitController.java
@@ -1,18 +1,25 @@
 package com.atguigu.springcloud.alibab.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+
         return "-------testA";
     }
 
     @GetMapping("/testB")
     public String testB(){
+        log.info( "线程池："+Thread.currentThread().getName()+"PaymentInfo_ok,id");
         return "-------testB";
     }
 }
