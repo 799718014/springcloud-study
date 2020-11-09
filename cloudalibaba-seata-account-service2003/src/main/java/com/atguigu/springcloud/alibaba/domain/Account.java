c940e94daf669b4a3cf1774a69dde977720f7425
liuzx <liuzx@usoftchina.com>
2020-11-09 23:06:55 +0800

    1、seata-account模块

---
+++ cloudalibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/domain/Account.java
@@ -0,0 +1,58 @@
+package com.atguigu.springcloud.alibaba.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    private Long id;
+    private Long userId;
+    private BigDecimal total;
+    private BigDecimal used;
+    private BigDecimal residue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public BigDecimal getUsed() {
+        return used;
+    }
+
+    public void setUsed(BigDecimal used) {
+        this.used = used;
+    }
+
+    public BigDecimal getResidue() {
+        return residue;
+    }
+
+    public void setResidue(BigDecimal residue) {
+        this.residue = residue;
+    }
+}
